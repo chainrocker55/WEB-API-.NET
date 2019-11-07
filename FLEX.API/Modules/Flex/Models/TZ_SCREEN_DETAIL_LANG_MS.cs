9e67d04ce7e33440158b9a79e75f18e63cdecd9e
Athit Upakan <athit@itpc.co.th>
2019-11-07 18:07:21 +0700

    ScreenDetailLang

--- FLEX.API/Modules/Flex/Models/TZ_SCREEN_DETAIL_LANG_MS.cs
+++ FLEX.API/Modules/Flex/Models/TZ_SCREEN_DETAIL_LANG_MS.cs
@@ -5,13 +5,9 @@ namespace FLEX.API.Models
 {
     public class TZ_SCREEN_DETAIL_LANG_MS
     {
-        [Required, MaxLength(240)]
         public string CONTROL_CD { get; set; }
-        [Required, MaxLength(40)]
         public string LANG_CD { get; set; }
-        [Required, MaxLength(120)]
         public string SCREEN_CD { get; set; }
-        [MaxLength(400)]
         public string CONTROL_CAPTION { get; set; }
         public string CRT_BY { get; set; }
         public DateTime CRT_DATE { get; set; }
